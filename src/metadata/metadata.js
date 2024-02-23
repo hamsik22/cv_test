6d272ffe2036394629c4c6632e1e28cc0656bc93
HamSik <109560875+hamsik22@users.noreply.github.com>
2024-02-23 16:43:58 +0900

    Update metadata.js

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -1,235 +1,57 @@
 module.exports = {
-  name: '하 지양',
-  eng_name: 'haji',
-  title: 'Data Engineer',
-  lang:'kor',
+  name: '황석현',
+  engName: 'SukHyeon Hwang',
+  title: 'iOS Developer',
   facts: {
-    Residence: 'Jinju, South Korea',
-    LinkedIn: '<a href="http://www.linkedin.com/in/haji8"><i class="fab fa-linkedin fact-icon"></i> haji8 </a>',
-    GitHub: '<a href="https://github.com/haji8-de"><i class="fab fa-github fact-icon"></i> haji8-de </a>',
-    Email: '<a href="mailto:gwd7454@gmail.com">gwd7454@gmail.com</a>',
-     'English CV':'<a href="https://haji8-de.github.io/cv_eng/"><i class="fab fa-file-text-o fact-icon"></i>CV_Eng</a>',
-    Portfolio: '<a href="https://haji8-de.github.io/portfolio/"> Portfolio </a>',
+    'Residence': '<a href="https://maps.app.goo.gl/A3iqDJAXEQZDpvve9"><i class="fa fa-home fact-icon"></i>Jinju, Korea</a>',
+    'Github': '<a href="https://github.com/hamsik22"><i class="fa-brands fa-github fact-icon"></i>Hamsik22</a>',
+    'Email': '<a href="mailto:payback9488@gmail.com"><i class="fa fa-envelope fact-icon"></i>payback9488@gmail.com</a>'
   },
   skills: [
-    ['python', '+++'],
-    ['Javascript', '++'],
-    ['ElasticSearch', '+++'],
-    ['AWS', '++'],
-    ['Firehose', '+++'],
-    ['QuickSight', '+++'],
-    
-    
-    ['flask', '++'],
-    ['React', '+'],
-    ['graphql', '++'],
-    ['', ''],
-    ['Athena', '+++'],
-    ['SageMaker', '+++'],
-    
-    ['django', '++'],
-    ['Vue.js', '++'],
-    ['', ''],
-    ['GCP', '+'],
-    ['', ''],
-    ['', ''],
-    
-    ['Hadoop', '++'],
-    ['hive', '++'],
-    ['impala', '++'],
-    ['Mysql', '+++'],
-    ['MongoDB', '++'],
-    ['postgrsql', '++'],
-    
-    ['Kafka', '++'],
-    ['airflow', '++'],
-    ['Flume', '++'],
-    ['pyflink', '++']
-    
-    
+    ['Swift', 80],
+    ['SwiftUI', 50],
+    ['테스트', 50]
   ],
   positions: [
     {
-      company: 'The HAJI',
-      location: 'Gyeongsangdo',
-      period: 'Sep 2022 - Present',
-      contents: `자체 소프트웨어 개발 및 외주`,
-      detail: [
-        'LG - ElasticSearch 1.4 data export batch',
-        'react hospital dashboard 구현',
-        'react 교육 및 개발자 영입',
-        '정부 지원 사업 계획서 작성 - 하우 유어 비자 (하유비)'
-      ],
+      title: '테스트',
+      period: '테스트 early 17th century',
+      skills: ['테스트', 'philosophy', 'death'],
+      contents: `
+테스트 Hamlet is visited by his father's ghost and ordered to avenge his father's murder by killing King Claudius, his uncle. After struggling with several questions, including whether what the ghost said is true and whether it is right for him to take revenge, Hamlet, along with almost all the other major characters, is killed.
+`
     },
     {
-      company: 'Korea Data Space',
-      location: 'Gangnam',
-      title: 'Data Enginner',
-      period: 'Jun 2022 - Sep 2022',
-      skills: ['ElasticSearch', 'pyflink', 'GCP'],
-      contents: `vision을 활용한 공간 정보 AI 연구소`,
-      detail: [
-        'ElasticSearch를 활용한 대시보드 개편',
-        'pyflink data pipeline 구축'
-      ],
+      title: 'Twelfth Night',
+      period: '1600 – 1601',
+      skills: ['comedy', 'love'],
+      contents: `
+Viola finds herself shipwrecked in Illyria and, assuming that her brother Sebastian has died in the wreck, disguises herself as a man to gain a position in Duke Orsino's court. Orsino sends Viola (whom he knows as Cesario) to deliver a message to his love, Olivia. Olivia, however, dislikes the Duke. She falls in love with Viola, who she thinks is a man. Eventually, Viola's brother Sebastian, who in fact was unharmed in the wreck, reappears. At a critical moment, Viola's true identity is revealed when members of the court notice the similarities between her and Sebastian. Olivia quickly falls in love with Sebastian, and Viola confesses her love for the Duke.
+`
     },
     {
-      company: 'Looko',
-      location: 'Gangnam',
-      title: 'Data Enginner',
-      period: 'Sep 2021 - Jun 2022',
-      skills: ['python', 'flask', 'Vue.js', 'mongodb', 'mariadb', 'postgrsql', 'Javascript'],
-      contents: `패션 AI 모델을 연구하고 고도화 하는 AI팀`,
-      detail: [
-        'AI 학습을 위한 데이터 라벨링, 검증 백오피스 개발',
-        'image type 판별/ background 제거 모델 고도화',
-        'Fashion item attribute classification model training image increasing for improve model performance'
-      ],
-    },
-    {
-      company: 'BALAAN',
-      location: 'Nonhyeon',
-      title: 'Data Developer',
-      period: 'may 2020 - Jul 2021',
-      skills: ['JavaScript', 'NodeJs', 'mongodb', 'mysql', 'typescript','python','ElasticSearch','AWS QuickSight','AWS Data services'],
-      contents: `발란 전사 데이터를 관리하는 데이터팀`,
-      detail: [
-        'OKR 대시보드 구축을 통한 매출 관리 포인트 강화',
-        '검색 랭킹 기능 등 검색 기능 개선을 통한 사용자 편의 제공',
-        '사용자 검색 데이터 등을 신규 AWS 파이프라인으로 구축'
-      ],
-    },
-    {
-      company: 'NAVER Corp',
-      location: 'Bundang',
-      title: 'Data Engineer',
-      period: 'Jul 2016 - Aug 2018',
-      skills: ['Python','django','hadoop','kafka','flume','logstash','hive','ElasticSearch','spark','java'],
-      contents: `네이버 전사 동영상 로그를 수집하는 동영상 플랫폼팀 데이터 파트`,
-      detail: [
-        'ElasticSearch를 이용한 실시간 통계 파이프라인 구축',
-        '동영상 재생 데이터 등을 통계 대시보드로 구현',
-        '데이터 지표 확장을 통한 분석 포인트 증가',
-        '수집 데이터 전수 검사를 통한 데이터 정합성 향상',
-        '재생 패턴에 따라 어뷰징 필터링 시스템 강화'
-      ],
-    },
-    {
-      company: 'WatchTek',
-      location: 'Yeouido',
-      title: 'Java Software Engineer',
-      period: 'Mar 2014 - Apr 2016',
-      skills: ['java','mybatis','spring','flex','sencha touch', 'codova', 'oracle','mysql','junit','Jenkins','Sonar Qube', 'MediaWiki'],
-      contents: `관제 시스템의 코어부를 담당하는 신기술 파트`,
-      detail: [
-        'watchall 10의 공통 프로세스 이슈 처리',
-        '물리 네트워크를 네트워크 정보를 통해 연결 정보를 알아내는 선번장 개발(특허)',
-        '소스 품질 관리 시스템, WIKI 구축',
-        '개발팀 신기술 파트 리더'
-      ],
-    },
-  ],
-  education:[
-    {
-      period:'2010 - 2014',
-      university:'Kumoh National Institute of Technology',
-      major:'Computer Software Engineering'
+      title: 'The Tragedy of Romeo and Juliet',
+      period: '1595–1596, with a possible early draft written in 1591',
+      skills: ['tragedy', 'death', 'love'],
+      contents: `
+In Verona, Italy, two families, the Montagues and the Capulets, are in the midst of a bloody feud. Romeo, a Montague, and Juliet, a Capulet, fall in love and struggle to maintain their relationship in the face of familial hatred. After Romeo kills Juliet's cousin Tybalt in a fit of passion, things fall apart. Both lovers eventually commit suicide within minutes of each other, and the feuding families make peace over their recent grief.
+`
     }
   ],
-  release:[
+  experience: [
     {
-      type:'publications',
-      title:'The HAJI : a language learning and career development space',
-      url:'https://tcs-asia.org/en/programs/program.php?forums=F1560230836&year=2023',
-      date:'May 2023, Trilateral Entrepreneurs Forum - 2023'
+      header: 'Performance',
+      skills: ['theater', 'play'],
+      contents: `
+Thousands of performances of my plays have been staged since the end of the 16th century. Many of my greatest plays were performed by the Lord Chamberlain's Men and King's Men acting companies at the [Globe](https://en.wikipedia.org/wiki/Globe_Theatre) and Blackfriars Theatres.
+`
     },
     {
-      type:'publications',
-      title:'가심비 AWS ElasticSearch Architecture',
-      url:'https://medium.com/@hajix/elasticsearch-8-2-%EC%84%A4%EC%B9%98-1c4149b92b3b',
-      date:'Jul 2022, aws.architecture.meetup.34회'
-    },
-    {
-      type:'publications',
-      title:'커머스 스타트업의 효율적인 데이터 분석 플랫폼 구축기',
-      url:'https://www.youtube.com/watch?v=j7O9RZDsacY',
-      date:'May 2021, AWS Summit Online Korea 2021'
-    },
-    {
-      type:'patent',
-      title:'Resource information management and data storage system through the management of the resource template',
-      url:'http://kpat.kipris.or.kr/kpat/biblioa.do?method=biblioFrame&start=biblio&searchFg=N',
-      date:'Dec 2015',
-    },
-    {
-      type:'thesis',
-      title:'PC Virtual I/O Device Development using Android Smartphone',
-      url:'https://www.eiric.or.kr/literature/ser_view.php?SnxGubun=INKO&mode=total&searchCate=literature&gu=INME001E9&cmd=qryview&SnxIndxNum=152361&rownum=&totalCnt=1&rownum=1&q1_t=7JWI65Oc66Gc7J2065OcIOyKpOuniO2KuO2PsOydhCDsnbTsmqntlZwgUEMg6rCA7IOBIOyehey2nOugpSDs',
-      date:'Jan 2013',
+      header: 'Poetry',
+      skills: ['poetry', 'erotic'],
+      contents: `
+In 1593 and 1594, when the theatres were closed because of plague, I published two narrative poems on sexual themes, Venus and Adonis and The Rape of Lucrece. I dedicated them to Henry Wriothesley, Earl of Southampton.
+`
     }
-  ],
-  certifications:[
-    {
-      icon:'',
-      url:'https://youth.posco.com/posco/edu/index.php?mod=incubator&pag=incubator01',
-      title:'POSCO Startup Incubating School',
-      major:'Apr 2023'
-    },
-    {
-      icon:'',
-      url:'https://coursera.org/share/a3eec63e1a35bcc206a98a58567e0b2d',
-      title:'Coursera ML course',
-      date:'Oct 2021'
-    },
-    {
-      icon:'',
-      url:'https://www.tdisdi.com/sdi/get-certified/divemaster-course/',
-      title:'SDI DIVE MASTER',
-      date:'Mar 2020'
-    },
-    {
-       icon:'',
-       url:'',
-       title:'JPT 575/990',
-       date:'Oct 2018'
-    },
-    {
-      icon:'',
-      url:'',
-      title:'OPIC IL',
-      date:'Jan 2014'
-    },
-    {
-      icon:'',
-      url:'http://www.q-net.or.kr/crf005.do?id=crf00503&jmCd=1320&gSite=Q&gId',
-      title:'정보처리기사',
-      date:'Nov 2013'
-    },
-  ],
-  addExperience:[
-    {
-      type: 'theater',
-      title: '극단 빈공간, 한 여름밤의 꿈, 스누그역',
-      url: 'https://www.youtube.com/watch?v=gLPWCMX1eKs',
-      date: 'Aug 2015',
-    },
-    {
-      type: 'activity',
-      title: '제 2 대 컴퓨터 소프트웨어 공학과 학생회, 회장',
-      url: '',
-      date: 'Mar 2013 ~ Feb 2014',
-    }
-  ],
-  award:[
-    {
-      title: '캡스톤 디자인 경진 대회 금상-Bubble Finder',
-      url:'',
-      date:'Nov 2012',
-    },
-    {
-      title: '캡스톤 디자인 경진 대회 장려-RMD for Android',
-      url:'http://gmilbo.net/news/article.html?no=21412',
-      date:'Nov 2011',
-    },
   ]
 };
